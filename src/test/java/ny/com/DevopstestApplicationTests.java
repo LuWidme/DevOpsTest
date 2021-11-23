736e3a3c234db8f06824daf6f9c325aa021bc3cf
Luca <luca.widmer@noseryoung.ch>
2021-11-23 11:45:17 +0100

    added tests

--- src/test/java/ny/com/DevopstestApplicationTests.java
+++ src/test/java/ny/com/DevopstestApplicationTests.java
@@ -1,7 +1,9 @@
 package ny.com;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.ResponseEntity;
 
 @SpringBootTest
 class DevopstestApplicationTests {
@@ -9,5 +11,9 @@ class DevopstestApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	public void doTest() {
+		TestContoller testContoller=new TestContoller();
+		Assertions.assertEquals( ResponseEntity.ok().body("Docker is Easy! :D"), testContoller.HomeTest() );
+	}
 }
