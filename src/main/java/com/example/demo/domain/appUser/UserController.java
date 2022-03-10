3cc810fe6c3d10c74c0d2525d02328e6a7209922
Luca <luca.widmer@noseryoung.ch>
2022-03-10 11:28:53 +0100

    test deployment of h2 BE to heroku

---
+++ src/main/java/com/example/demo/domain/appUser/UserController.java
@@ -0,0 +1,31 @@
+package com.example.demo.domain.appUser;
+
+
+import io.swagger.v3.oas.annotations.Operation;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Collection;
+import java.util.UUID;
+
+@RestController @RequestMapping("/api")
+@RequiredArgsConstructor
+public class UserController {
+//    ADD YOUR ENDPOINT MAPPINGS HERE
+private final UserService userService;
+
+    @GetMapping("/")
+    public ResponseEntity<String> HomeTest(){
+        return ResponseEntity.ok().body("Hello World");
+    }
+
+    @Operation(summary = "Shows all Users, Top Secret")
+    @GetMapping("/users")
+    public ResponseEntity<Collection<User>> findAll() {
+        return new ResponseEntity<Collection<User>>(userService.findAll(), HttpStatus.OK);
+    }
+
+}
